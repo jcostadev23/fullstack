ba74fa3c45227518f7ecde27ef63ca3bb3ad7736
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-08-24 12:28:36 +0100

    feat(modalComponent)

---
+++ client/src/components/Modal.js
@@ -0,0 +1,27 @@
+class Modal {
+  constructor() {
+    this._modal = document.getElementById('modal');
+    this._modalBtn = document.getElementById('modal-btn');
+    this.addEventListeners();
+  }
+  open() {
+    this._modal.style.display = 'block';
+  }
+
+  close() {
+    this._modal.style.display = 'none';
+  }
+
+  closeModal(e) {
+    if (e.target === this._modal) {
+      this.close();
+    }
+  }
+
+  addEventListeners() {
+    this._modalBtn.addEventListener('click', this.open.bind(this));
+    window.addEventListener('click', this.closeModal.bind(this));
+  }
+}
+
+export default Modal;
