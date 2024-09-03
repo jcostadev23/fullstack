dda36e5bb78e9d806d0b4e4f1785288701f701a2
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-09-03 13:27:24 +0100

    feat(listComponent)

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import '@fortawesome/fontawesome-free/css/all.css';
-import Modal from './components/Modal';
+import IdeasList from './components/IdeasList';
 import InputForm from './components/InputForm';
+import Modal from './components/Modal';
 import './css/style.css';
+import { getIdeas } from './utils/getData';
 
 const modal = new Modal();
 const inputForm = new InputForm();
 inputForm.render();
+
+async function getData() {
+  try {
+    const result = await getIdeas();
+    const ideasList = new IdeasList(result.data);
+    ideasList.render();
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+getData();
