19314df4f26f8211e484cf586b9e529d27a8bab8
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-09-05 21:32:17 +0100

    feat(deleteIdea)

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import '@fortawesome/fontawesome-free/css/all.css';
+import DeleteIdea from './components/DeleteIdea';
 import IdeasList from './components/IdeasList';
 import InputForm from './components/InputForm';
 import Modal from './components/Modal';
-import './css/style.css';
 import { getIdeas } from './utils/apiCalls';
+import './css/style.css';
 
 const modal = new Modal();
 const inputForm = new InputForm();
+const deleteIdea = new DeleteIdea();
 
 (async () => {
   const result = await getIdeas();
@@ -17,5 +19,8 @@ const inputForm = new InputForm();
   const ideasList = new IdeasList(result.data);
   ideasList.render();
 })();
+
+const ideaListElement = document.getElementById('idea-list');
+const event = new DeleteIdea(ideaListElement);
 
 inputForm.render();
